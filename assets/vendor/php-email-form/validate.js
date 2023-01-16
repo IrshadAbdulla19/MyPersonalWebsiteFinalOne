99a716df47fd7cfecdc029febd4f97eed28066f5
IrshadAbdulla19 <122153626+IrshadAbdulla19@users.noreply.github.com>
2023-01-16 19:47:17 +0530

    Add files via upload

---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,104 @@
+/**
+* PHP Email Form Validation - v3.5
+* URL: https://bootstrapmade.com/php-email-form/
+* Author: BootstrapMade.com
+*/
+(function () {
+  "use strict";
+
+  let forms = document.querySelectorAll('.php-email-form');
+
+  forms.forEach( function(e) {
+    e.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      let thisForm = this;
+
+      let action = thisForm.getAttribute('action');
+      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+      
+      if( ! action ) {
+        displayError(thisForm, 'The form filling is done ')
+        return;
+      }
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+      let formData = new FormData( thisForm );
+
+      if ( recaptcha ) {
+        if(typeof grecaptcha !== "undefined" ) {
+          grecaptcha.ready(function() {
+            try {
+              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+              .then(token => {
+                formData.set('recaptcha-response', token);
+                php_email_form_submit(thisForm, action, formData);
+              })
+            } catch(error) {
+              displayError(thisForm, error)
+            }
+          });
+        } else {
+          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+        }
+      } else {
+        php_email_form_submit(thisForm, action, formData);
+      }
+    });
+  });
+
+  function php_email_form_submit(thisForm, action, formData) {
+    fetch(action, {
+      method: 'POST',
+      body: formData,
+      headers: {'X-Requested-With': 'XMLHttpRequest'}
+    })
+    .then(response => {
+      return response.text();
+    })
+    .then(data => {
+      thisForm.querySelector('.loading').classList.remove('d-block');
+      if (data.trim() == 'OK') {
+        thisForm.querySelector('.sent-message').classList.add('d-block');
+        thisForm.reset(); 
+      } else {
+        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+      }
+    })
+    .catch((error) => {
+      displayError(thisForm, error);
+    });
+  }
+
+  function displayError(thisForm, error) {
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+
+})();
+function validationDetiles(){
+  let btn;
+  var name=document.getElementById("name").value.trim()
+  var mail=document.getElementById("email").value.trim()
+  var subject=document.getElementById("subject").value.trim()
+
+  if(name==""||email==""||subject==""){
+    alert("please fill all fields")
+  }else if(!/^([a-zA-Z0-9\_.]+)@([a-zA-Z0-9-]+).([a-z0-9]+)?$/.test(mail)){
+      alert("Please re-check at email")
+      btn=false
+ }else if(!/^[a-zA-Z]*$/.test(name)){
+    alert("Please enter your name correctly")
+    btn=false
+ }else if(!/^[a-zA-Z]*$/.test(subject)){
+    alert("Please enter valid subject input")
+    btn=false
+ }
+
+return btn;
+  
+}
+
